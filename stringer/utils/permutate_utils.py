efd3acc024307bc81718971b457accba214ba792
Kalab Oster <kalaboster@gmail.com>
2017-10-10 13:53:55 -0700

    Write basic perm function and test to check the three given strings. I will test more, but right now I will publish and move to write the CLI.

--- stringer/utils/permutate_utils.py
+++ stringer/utils/permutate_utils.py
@@ -1,1 +1,22 @@
-__author__ = 'kalab'
+'''
+A utility class for functions to permutate tools
+'''
+import logging
+from itertools import permutations
+
+def generate(string=None):
+    logging.debug("generate permutations with string: " + string)
+
+    # Create list for permutations.
+    permutate_list = []
+
+    if string is None:
+        logging.error('generate string is ' + None)
+    else:
+        logging.debug("generating permutations.")
+
+
+        permutate_list = [''.join(value) for value in permutations(string)]
+
+
+    return permutate_list
--- test/utils/test_permutate_utils.py
+++ test/utils/test_permutate_utils.py
@@ -1,1 +1,15 @@
-__author__ = 'kalab'
+'''
+Tests for permutations.
+'''
+import stringer.utils.permutate_utils as perm
+
+def test_permutations_short_string():
+
+    perm_list1 = perm.generate("aaa")
+    perm_list2 = perm.generate("cake")
+    perm_list3 = perm.generate("banana")
+
+    assert ['aaa', 'aaa', 'aaa', 'aaa', 'aaa', 'aaa'] == perm_list1
+    assert ['cake', 'caek', 'ckae', 'ckea', 'ceak', 'ceka', 'acke', 'acek', 'akce', 'akec', 'aeck', 'aekc', 'kcae',
+            'kcea', 'kace', 'kaec', 'keca', 'keac', 'ecak', 'ecka', 'eack', 'eakc', 'ekca', 'ekac'] == perm_list2
+    assert "anaban" == perm_list3[714]
