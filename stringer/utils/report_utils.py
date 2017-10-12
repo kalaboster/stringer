046a9ea821051b9a9e328cf4e926ac40faf9e648
Kalab Oster <kalaboster@gmail.com>
2017-10-12 11:42:10 -0700

    Write function and test to collect meta data on any file given and return a hashmap. This will be good for the next version. Might not need this now. Publish to focus on the entire process to see where the report exists.

--- stringer/utils/report_utils.py
+++ stringer/utils/report_utils.py
@@ -1,1 +1,40 @@
-__author__ = 'kalab'
+# Open Story License
+#
+# Story: stringer
+# Writer: Kalab J. Oster(TM)
+# Copyright Holders: Kalab J. Oster(TM)
+# copyright (C) 2017 Kalab J. Oster(TM)
+#
+# Permission is granted by the Copyright Holders for humans or other intelligent agents to read, write, edit, publish and critique the Story
+# if the humans or intelligent agents keep this Open Story License with the Story,
+# and if the Story you tell remains free,
+# and if another writer writes or edits the Story then the writer's name needs to be appended to the end of the Writer list of this Open Story License.
+import logging
+import os
+
+
+'''
+Future use to get more data on the file before masking.
+'''
+def get_file_meta_dict(file):
+    logging.debug("get_file_meta_dict")
+
+    file_meta_dict = {}
+
+    if file is None or os.path.isfile(file) is False:
+        logging.error("if file is None or os.path.isfile(file) is False")
+    else:
+
+        file_name = os.path.basename(file)
+        os_stat = os.stat(file)
+
+        file_meta_dict = {'file_name': file_name, 'os_stat': str(os_stat)}
+
+
+    return file_meta_dict
+
+
+
+
+
+    return is_file
--- test/utils/test_report_utils.py
+++ test/utils/test_report_utils.py
@@ -1,1 +1,25 @@
-__author__ = 'kalab'
+# Open Story License
+#
+# Story: stringer
+# Writer: Kalab J. Oster(TM)
+# Copyright Holders: Kalab J. Oster(TM)
+# copyright (C) 2017 Kalab J. Oster(TM)
+#
+# Permission is granted by the Copyright Holders for humans or other intelligent agents to read, write, edit, publish and critique the Story
+# if the humans or intelligent agents keep this Open Story License with the Story,
+# and if the Story you tell remains free,
+# and if another writer writes or edits the Story then the writer's name needs to be appended to the end of the Writer list of this Open Story License.
+import stringer.utils.report_utils as report_utils
+import os
+
+TEST_FILE = os.path.dirname(__file__) + "/../files/test_log.txt"
+
+TEST_FILE_META_DICT = {'os_stat': "posix.stat_result(st_mode=33204, st_ino=4199090, st_dev=2051L, st_nlink=1, st_uid=1001, "
+                                  "st_gid=1001, st_size=10381, st_atime=1507754550, st_mtime=1507656962, st_ctime=1507656962)", 'file_name': 'test_log.txt'}
+
+def test_get_file_meta_dict():
+
+    is_file = report_utils.get_file_meta_dict(TEST_FILE)
+
+    assert  TEST_FILE_META_DICT == is_file
+
