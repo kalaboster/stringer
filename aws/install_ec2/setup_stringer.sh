dd92f49233057b1d5c36893060636bb063af2c87
Kalab Oster <kalaboster@gmail.com>
2017-10-19 10:23:35 -0700

    Write enough to begin to test the code deploy beyond download. ...Publish to get the code that is available to AWS.

--- aws/install_ec2/setup_stringer.sh
+++ aws/install_ec2/setup_stringer.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 pip install -r requirements.txt
-mkdir /var/www
-mkdir /var/www/stringer
-cp -R . /var/www/stringer/
-chown -R ubuntu:ubuntu /var/www/stringer
+
+# Test to verify privledge levels.
+mkdir /var/test
+cp -R . /var/test/
+
 
