7b00c073ac3041edad527b77c525a825e4703b61
Kalab Oster <kalaboster@gmail.com>
2017-10-19 09:39:17 -0700

    Edit to have both processes in their respective shell scripts. Publish to save space.

--- aws/install_ec2/setup_nginx.sh
+++ aws/install_ec2/setup_nginx.sh
@@ -1,1 +1,13 @@
 #!/usr/bin/env bash
+sudo rm /etc/nginx/sites-enabled/default
+
+sudo ln -s /var/www/stringer/install/stringer_nginx.conf /etc/nginx/conf.d/
+
+
+
+sudo service nginx restart
+
+
+sudo mkdir -p /var/log/uwsgi
+sudo chown -R ubuntu:ubuntu /var/log/uwsgi
+uwsgi --ini /var/www/stringer/install/stringer_uwsgi.ini
--- aws/install_ec2/setup_stringer.sh
+++ aws/install_ec2/setup_stringer.sh
@@ -1,21 +1,8 @@
 #!/usr/bin/env bash
 
-
 pip install -r requirements.txt
-sudo mkdir /var/www
-sudo mkdir /var/www/stringer
+mkdir /var/www
+mkdir /var/www/stringer
 cp -R . /var/www/stringer/
-sudo chown -R ubuntu:ubuntu /var/www/stringer
+chown -R ubuntu:ubuntu /var/www/stringer
 
-sudo rm /etc/nginx/sites-enabled/default
-
-sudo ln -s /var/www/stringer/install/stringer_nginx.conf /etc/nginx/conf.d/
-
-
-
-sudo service nginx restart
-
-
-sudo mkdir -p /var/log/uwsgi
-sudo chown -R ubuntu:ubuntu /var/log/uwsgi
-uwsgi --ini /var/www/stringer/install/stringer_uwsgi.ini
