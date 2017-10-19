3588fb5f923e94df5371c444ced880662c48f525
Kalab Oster <kalaboster@gmail.com>
2017-10-19 09:55:22 -0700

    Edit to remove the thought of having stringer be its own service. I want nginx as a proxy for now. I am going to commit these changes as they contain things I wrote through process, and I want to at least having codedeploy setting up stringer on the ec2 instance even if I have to manuall start the service.

--- aws/install_ec2/setup_nginx.sh
+++ aws/install_ec2/setup_nginx.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
+
+# TODO: Get codedeploy working with setup_stringer.sh then focus on this file related to setup_nginx.sh.
 sudo rm /etc/nginx/sites-enabled/default
-
 sudo ln -s /var/www/stringer/install/stringer_nginx.conf /etc/nginx/conf.d/
-
-
-
 sudo service nginx restart
-
-
 sudo mkdir -p /var/log/uwsgi
 sudo chown -R ubuntu:ubuntu /var/log/uwsgi
 uwsgi --ini /var/www/stringer/install/stringer_uwsgi.ini
